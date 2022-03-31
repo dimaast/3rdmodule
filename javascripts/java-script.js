d7ad981174233188a1ab9e26de59029a855f9e6c
dima <wxllfwlr@gmail.com>
2022-03-31 16:09:38 +0300

    change all

--- javascripts/java-script.js
+++ javascripts/java-script.js
@@ -9,28 +9,56 @@ $(document).ready(function(){
 $(".activeelement").mouseover(function(){
   $(".cursor").css("transform", "scale(0.4)", 0.5)
   });
-$("a").mouseover(function(){
+$(".activeelement").mouseover(function(){
   $(".cursor").css("border", "2px solid", 0.5)
     });
-$("a").mouseleave(function(){
+$(".activeelement").mouseleave(function(){
   $(".cursor").css("border", "1px solid", 0.5)
     });
-$("a").mouseleave(function(){
+$(".activeelement").mouseleave(function(){
   $(".cursor").css("transform", "none", 0.5)
     });
 });
 
-$(document).ready(function(){
-$(".block0").hover(function(){
-  $(".block").addClass("block1");
-}, function() {
-  $(".block").removeClass("block1");
-    });
+$(document).ready(function () {
+  $('.buttonnachat').click(function() {
+    $('html, body').animate({
+      scrollTop: $(".secondslide").offset().top
+    }, 1000);
+  });
+});
+
+let i = 0;
+let txt = 'Меня научили менять цвет, прямо как хамелеон!';
+let speed = 60;
+$(document).ready(function textstring() {
+$(document).ready(function typeWriter() {
+autoStart: false;
+if (i < txt.length) {
+document.getElementById("d2bubble1text").innerHTML += txt.charAt(i);
+i++;
+setTimeout(typeWriter, speed);
+}
+});
 });
 
 $(document).ready(function(){
-	$(".block0").click(function(){
-		$(".block").toggleClass("block2");
-	});
-
+  $(".d2rabbit").click(function(){
+    if ($(".d2rabbit").hasClass("rv1")){
+      $(".d2rabbit").addClass("rv2");
+      setTimeout(function(){
+        $(".d2rabbit").removeClass("rv1");
+      }, 100);
+    } else if ( $(".d2rabbit").hasClass("rv2")){
+      $(".d2rabbit").addClass("rv3");
+      setTimeout(function(){
+        $(".d2rabbit").removeClass("rv2");
+      }, 100);
+    } else if ( $(".d2rabbit").hasClass("rv3")){
+      $(".d2rabbit").addClass("rv1");
+      setTimeout(function(){
+        $(".d2rabbit").removeClass("rv3");
+      }, 100);
+    };
+  });
 });
